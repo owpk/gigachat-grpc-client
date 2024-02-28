3495fce32932a1961949f68c8b7fcd38c2a584fd
Vyacheslav Vorobev <voyzvz@gmail.com>
2024-02-28 15:54:37 +0800

    MAIN: update install script, add build option '-b'

--- install.sh
+++ install.sh
@@ -1,25 +1,30 @@
 #!/bin/bash 
 
-git pull
-
 TARGET=~/.local/bin/gigachat
-
 VERSION=$( curl -I https://github.com/owpk/gigachat-grpc-client/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}' )
+LAST_VER=$( gigachat -v 2> /dev/null )
 
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-
-LAST_VER=$( gigachat -v 2> /dev/null )
 
 if [[ -z "$LAST_VER" || $(version $VERSION -ge version $LAST_VER) ]]; then
     rm $TARGET 2> /dev/null
     echo "------"
     echo "Found newest version!: $VERSION"
-    echo "Downloading..."
     echo "------"
-    REDIRECT=$( curl -I "https://github.com/owpk/gigachat-grpc-client/releases/download/$VERSION/gigachat" \
-        | awk '/^location/ {print $2}' | sed -e 's/\n//g' | sed -e 's/\r//g')
-    curl $REDIRECT --output $TARGET
-    chmod +x $TARGET
+
+    if [ "$1" == "-b" ]; then
+      git pull
+      echo "Building..."
+      ./gradlew nativeBuild --info
+      cp ./build/native/nativeCompile/gigachat $TARGET
+    else
+      echo "Downloading..."
+      REDIRECT=$( curl -I "https://github.com/owpk/gigachat-grpc-client/releases/download/$VERSION/gigachat" \
+          | awk '/^location/ {print $2}' | sed -e 's/\n//g' | sed -e 's/\r//g')
+      curl $REDIRECT --output $TARGET
+      chmod +x $TARGET
+    fi
+
     exit 0
 fi
 
